faa498e093caeb0b6e9088aec33fa93b533c37a0
scooby78 <scooby78@users.noreply.github.com>
2016-02-21 12:47:34 +1000

    Modified For Adv Medical
    Files moved to correct folders for packing into PBO as a standalone mod

---
+++ mod.cpp
@@ -0,0 +1,8 @@
+name = "160th SOR Loadout Mod Desert Uniform Advanced Medical";
+picture = "160thdesert.paa";
+actionName = "Forum";
+action = "http://160th.net";
+logo = "160thmarine.paa";
+tooltip = "Desert Advanced Medical Loadout"; 
+overview = "160th loadout configurations and scripted objects with a Desert theme and ACE advanced medical system";
+author = "Scooby";
