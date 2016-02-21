7289309ac1016c1b1cfb467da48cd48cfe9973ec
scooby78 <scooby78@users.noreply.github.com>
2016-02-21 22:52:31 +1000

    PBO preparation

--- mod.cpp
+++ mod.cpp
@@ -1,4 +1,4 @@
-name = "160th SOR Desert loadout mod advanced medical patch";
+name = "160th SOR Desert Loadout Mod Advanced Medical Patch";
 picture = "160thdesert.paa";
 actionName = "Forum";
 action = "http://160th.net";
