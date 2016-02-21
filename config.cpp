4a9fc4fb93429a307fb81e2adb1c2bbc727cb0cc
scooby78 <scooby78@users.noreply.github.com>
2016-02-21 12:40:47 +1000

    Modified for Adv Med
    Removal of files not needed due to inheritance from SOR_Desert_Loadouts
    mod.

--- config.cpp
+++ config.cpp
@@ -5,12 +5,10 @@
 	Description: config.cpp
 */
 
-//#include "cfgFunctions.hpp"
 #include "define_macros.hpp"
 #include "cfgPatches.hpp"
 #include "cfgFactionClasses.hpp"
 #include "cfgVehicles.hpp"
-//#include "cfgVehicleClasses.hpp"
 #include "cfgGroups.hpp"
-//#include "cfgWeapons.hpp"
+
 
